d361811df6a1d8bf341dbb7ab4e78df9b63224b9
pavankumar0611 <pavankohli0644@gmail.com>
2026-01-05 02:09:49 -0800

    Added current directory(cd) while running exectuable

--- running_exectubale.c
+++ running_exectubale.c
@@ -1,35 +1,58 @@
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
 int main()
 {
-        pid_t pid;
-        char *args[2];
+	pid_t pid;
+	char *args[2];
+	char size = 100;
+	char buffer[size];
 
-        while(1)
-        {
+	while(1)
+	{
+		char string[100];
 
-                pid = fork();
-                if(pid == 0) {
-                        char string[10];
-                        printf("Enter the executable you want to run\npavan>");
-                        fgets(string , sizeof(string), stdin);
+		if(getcwd(buffer, size) != NULL) 
+			printf("Enter the executable you want to run\npavan:<%s>$", buffer);
 
-                        string[strcspn(string, "\n")] = '\0';
+		fgets(string , sizeof(string), stdin);
 
-                        args[0] = string;
-                        args[1] = NULL;
+		string[strcspn(string, "\n")] = '\0';
 
-                        execv(args[0] , args);
-                }
-                else if (pid > 0) {
-                        wait(NULL);
-        }
-                else
-                        printf("still running\n");
-        }
+		if(string[0] != 'c' || string[1] != 'd' ) {
+			pid = fork();
+			if(pid == 0) {
+				args[0] = string;
+				args[1] = NULL;
 
+				execv(args[0] , args);
+			}
+			else if(pid > 0)
+				wait(NULL);
+		}
+		else{
+			char *temp_string;
+
+			if ((temp_string = strchr(string, '/')) != NULL)
+			{
+				/* temp_string now points to '/' */
+			}
+			else
+				if (strncmp(string, "cd ", 3) == 0)
+					temp_string = string  + 3;
+
+			struct stat sb;
+
+			if (stat(temp_string, &sb) !=  0)
+				printf("Path is invalid\n");
+
+			chdir(temp_string);
+		}
+	}
 }
