e22242bec47f5aef67b93dcbcffc2cffa33e8db2
pavankumar0611 <pavankohli0644@gmail.com>
2026-01-05 21:15:13 -0800

    Added function to remove trailing space and minor changes.

--- running_exectubale.c
+++ running_exectubale.c
@@ -6,19 +6,28 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <ctype.h>
+
+#define SIZE 100
+
+void trim_trailing_space(char *s) {
+    int  len = strlen(s);
+    while (len > 0 && isspace((unsigned char)s[len - 1])) {
+        s[--len] = '\0';
+    }   
+}
 
 int main()
 {
 	pid_t pid;
 	char *args[2];
-	char size = 100;
-	char buffer[size];
+	char buffer[SIZE];
 
 	while(1)
 	{
-		char string[100];
+		char string[SIZE];
 
-		if(getcwd(buffer, size) != NULL) 
+		if(getcwd(buffer, SIZE) != NULL) 
 			printf("Enter the executable you want to run\npavan:<%s>$", buffer);
 
 		fgets(string , sizeof(string), stdin);
@@ -49,6 +58,7 @@ int main()
 
 			struct stat sb;
 
+			trim_trailing_space(temp_string);
 			if (stat(temp_string, &sb) !=  0)
 				printf("Path is invalid\n");
 
